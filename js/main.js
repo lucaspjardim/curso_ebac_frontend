8b701e4df3eb63400bfe54a8f42f17b91d5a442e
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-02-08 09:25:45 -0300

    Funcionando

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault()
+
+        const inputText = $('#text-input').val()
+        const newList = $(`<li></li>`)
+        $(`${inputText}`).appendTo(newList)
+        $(`<li>${inputText}</li>`).appendTo(newList)
+        $(newList).appendTo('ul')
+    })
+})
